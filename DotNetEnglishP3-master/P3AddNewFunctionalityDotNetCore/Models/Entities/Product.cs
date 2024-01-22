fda9cb408a7f0d47bc62be5c2424715cd13a8136
Thomas COLLET <thomas.collet@axa.fr>
2024-01-22 14:31:41 +0100

    Modification de la classe product pour afficher les messages d'erreurs lors de la validation du formulaire d'ajout de produits

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/Entities/Product.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/Entities/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.Entities
 {
@@ -12,8 +13,16 @@ namespace P3AddNewFunctionalityDotNetCore.Models.Entities
         public int Id { get; set; }
         public string Description { get; set; }
         public string Details { get; set; }
+
+        [Required(ErrorMessage = "ErrorMissingName")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price")]
+        [RegularExpression("^[0-9]*\\.?[0-9]+$", ErrorMessage = "Le champ prix doit être un nombre positif.")]
         public double Price { get; set; }
+
+        [Required(ErrorMessage = "ErrorMissingStock")]
+        [RegularExpression("^\\d+$", ErrorMessage = "Le champ Stock doit être un entier.")]
         public int Quantity { get; set; }
 
         public virtual ICollection<OrderLine> OrderLine { get; set; }
