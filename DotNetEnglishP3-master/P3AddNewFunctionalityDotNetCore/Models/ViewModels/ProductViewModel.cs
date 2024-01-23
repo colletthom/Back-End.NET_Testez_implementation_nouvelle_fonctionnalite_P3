58aa21b5d1b8cef526ef37f97867d33032a593bb
Thomas COLLET <thomas.collet@axa.fr>
2024-01-23 18:28:21 +0100

    réalisation du ToDO sur CheckProductErrors J'ai passé la journnée à résoudre le problème du Layout  qui n'a rien à voir avec le projet

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
 {
@@ -7,14 +8,20 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
         [BindNever]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "ErrorMissingName")]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Details { get; set; }
 
+        [Required(ErrorMessage = "ErrorMissingStock")]
+        [RegularExpression("^\\d+$", ErrorMessage = "Le champ Stock doit être un entier.")]
+
         public string Stock { get; set; }
 
+        [Required(ErrorMessage = "Price")]
+        [RegularExpression("^[0-9]*\\.?[0-9]+$", ErrorMessage = "Le champ prix doit être un nombre positif.")]
         public string Price { get; set; }
     }
 }
