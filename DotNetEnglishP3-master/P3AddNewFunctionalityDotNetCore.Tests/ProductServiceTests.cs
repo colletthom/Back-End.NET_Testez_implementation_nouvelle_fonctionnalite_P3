c41ec53f1095479dc919765c6af8f9d07b08908f
Thomas COLLET <thomas.collet@axa.fr>
2024-01-23 14:24:43 +0100

    Nouveaux essais pour lancer le test mais çà ne fonctionne pas

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -1,12 +1,39 @@
-﻿using P3AddNewFunctionalityDotNetCore.Models.Entities;
+﻿using Microsoft.Extensions.Localization;
+using Moq;
+using P3AddNewFunctionalityDotNetCore.Models;
+using P3AddNewFunctionalityDotNetCore.Models.Entities;
+using P3AddNewFunctionalityDotNetCore.Models.Repositories;
 using P3AddNewFunctionalityDotNetCore.Models.Services;
 using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Resources;
 using Xunit;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
+    [CollectionDefinition("ProductServiceCollection")]
+    public class ProductServiceCollection : ICollectionFixture<ProductServiceFixture> { }
+
+    public class ProductServiceFixture
+    {
+        public IProductService ProductService { get; }
+        //private readonly IStringLocalizer<ProductService> _localizer;
+        public ProductServiceFixture()
+        {
+            
+            var cart = new Mock<ICart>().Object;
+            var productRepository = new Mock<IProductRepository>().Object;
+            var orderRepository = new Mock<IOrderRepository>().Object;
+            var _localizer = new Mock<IStringLocalizer<ProductService>>().Object;
+            //_localizer = localizer;
+
+            ProductService = new ProductService(cart, productRepository, orderRepository, _localizer);
+        }
+    }
+
+    [Collection("ProductServiceCollection")]
+   
     public class ProductServiceTests
     {
         /// <summary>
@@ -14,29 +41,48 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         /// A test method must check if a definite method does its job:
         /// returns an expected value from a particular set of parameters
         /// </summary>
+        
+        private readonly IProductService _productService;
+        
+        public ProductServiceTests(ProductServiceFixture productServiceFixture)
+        {
+            _productService = productServiceFixture.ProductService;
+        }
+        /*
+        public ProductServiceTests(IProductService productService)
+        {
+            _productService = productService;
+        }*/
+
         [Fact]
-        TestMethod]
         [Description("je ne mets pas de nom dans le formulaire,« Veuillez saisir un nom » doit être retourné ")]
 
         public void TestNomVide()
         {
             // Arrange
-            private readonly IProductService productService;
-        //ProductService productService = new ProductService();
+                  
             ProductViewModel productWithoutNom = new ProductViewModel()
             {
-                Name = null,
                 Description = "test nom vide",
                 Stock = "5",
                 Price = "10"
             };
+                    
             // Act
-            List<string> messageProductWithoutName = productService.CheckProductModelErrors(productWithoutNom);
+            List<string> messageProductWithoutName = _productService.CheckProductModelErrors(productWithoutNom);
 
             // Assert
-            Assert.Equal(1, 1);
+            bool isContain = false;
+            for(int i = 0; i < messageProductWithoutName.Count; i++)
+            {
+                if (!messageProductWithoutName[i].Contains("Veuillez saisir un nom"))
+                    isContain = true;
+            }
+            if (isContain==false)
+            {
+                Assert.Fail("le test ne fonctionne pas");
+            }           
         }
-
         // TODO write test methods to ensure a correct coverage of all possibilities
     }
 }
