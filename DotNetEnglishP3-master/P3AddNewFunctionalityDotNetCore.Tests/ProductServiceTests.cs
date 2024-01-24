ec73ba6d9a6e684b53e7325c2bfabdb941016fb4
Thomas COLLET <thomas.collet@axa.fr>
2024-01-24 10:19:53 +0100

    sauvegarde du test name mais qui fonctione mal (il moq ce que je souhaite tester)

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.Extensions.Localization;
 using Moq;
-using P3AddNewFunctionalityDotNetCore.Models;
-using P3AddNewFunctionalityDotNetCore.Models.Entities;
 using P3AddNewFunctionalityDotNetCore.Models.Repositories;
 using P3AddNewFunctionalityDotNetCore.Models.Services;
 using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using P3AddNewFunctionalityDotNetCore.Models;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Resources;
 using Xunit;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
@@ -19,9 +17,11 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
     {
         public IProductService ProductService { get; }
         //private readonly IStringLocalizer<ProductService> _localizer;
+
+        //public ProductServiceFixture(IStringLocalizer<ProductService> localizer)
         public ProductServiceFixture()
         {
-            
+
             var cart = new Mock<ICart>().Object;
             var productRepository = new Mock<IProductRepository>().Object;
             var orderRepository = new Mock<IOrderRepository>().Object;
@@ -33,7 +33,7 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
     }
 
     [Collection("ProductServiceCollection")]
-   
+
     public class ProductServiceTests
     {
         /// <summary>
@@ -41,7 +41,7 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         /// A test method must check if a definite method does its job:
         /// returns an expected value from a particular set of parameters
         /// </summary>
-        
+
         private readonly IProductService _productService;
         
         public ProductServiceTests(ProductServiceFixture productServiceFixture)
@@ -60,28 +60,28 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         public void TestNomVide()
         {
             // Arrange
-                  
+
             ProductViewModel productWithoutNom = new ProductViewModel()
             {
                 Description = "test nom vide",
                 Stock = "5",
                 Price = "10"
             };
-                    
+
             // Act
             List<string> messageProductWithoutName = _productService.CheckProductModelErrors(productWithoutNom);
 
             // Assert
             bool isContain = false;
-            for(int i = 0; i < messageProductWithoutName.Count; i++)
+            for (int i = 0; i < messageProductWithoutName.Count; i++)
             {
                 if (!messageProductWithoutName[i].Contains("Veuillez saisir un nom"))
                     isContain = true;
             }
-            if (isContain==false)
+            if (isContain == false)
             {
                 Assert.Fail("le test ne fonctionne pas");
-            }           
+            }
         }
         // TODO write test methods to ensure a correct coverage of all possibilities
     }
