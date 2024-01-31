0b23b5b11cc208f4f55a336fbf69d5975c3011ef
Thomas COLLET <thomas.collet@axa.fr>
2024-01-31 09:46:55 +0100

    fusion sur dev de tous les test unitaires et les tests d'intégration de creation et de suppresion de produits

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.TestsIntegrat/ProductControllerTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.TestsIntegrat/ProductControllerTests.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Dapper;
 using P3AddNewFunctionalityDotNetCore.Data;
 using Microsoft.Extensions.Configuration;
+using NuGet.Configuration;
 
 
 namespace P3AddNewFunctionalityDotNetCore.TestsIntegrat
@@ -25,6 +26,8 @@ namespace P3AddNewFunctionalityDotNetCore.TestsIntegrat
             // Configurer le chemin d'accès de base pour la recherche de appsettings.json
             var basePath = Path.Combine(Directory.GetCurrentDirectory());
 
+            
+
             _configuration = new ConfigurationBuilder()
                 .SetBasePath(basePath)
                 .AddJsonFile("appsettings.json", optional: true)
@@ -32,11 +35,12 @@ namespace P3AddNewFunctionalityDotNetCore.TestsIntegrat
         }
 
         [Fact]
-        [Description("J’ajoute un produit côté admin, je vérifie qu’il est présent côté utilisateur avec chacune des données valides")]
-        public void AjoutProduitAuStock()
+        [Description("I add a product on the admin side, I check that it is present on the user side with each valid data")]
+        public void AddProductInstock()
         {
             //Arrange
             var cart = new Cart();
+
             // Récupérer la chaîne de connexion depuis appsettings.json
             var connectionString = _configuration.GetConnectionString("P3Referential");
 
@@ -87,12 +91,13 @@ namespace P3AddNewFunctionalityDotNetCore.TestsIntegrat
         }
 
         [Fact]
-        [Description("J’ajoute un produit côté admin, je le supprime et vérifie qu’il a bien été supprimés")]
+        [Description("\r\nI add a product on the admin side, I delete it and check that it has been deleted")]
 
-        public void SuppresionProduitAuStock()
+        public void DeleteProductInStock()
         {
             //Arrange
             var cart = new Cart();
+
             // Récupérer la chaîne de connexion depuis appsettings.json
             var connectionString = _configuration.GetConnectionString("P3Referential");
 
@@ -118,11 +123,9 @@ namespace P3AddNewFunctionalityDotNetCore.TestsIntegrat
                     Stock = "2",
                     Details = "test2",
                     Price = "2"
-                }) as RedirectToActionResult;
+                });
 
                 // Assert
-
-                //var productsAfterAdd = productService.GetAllProductsViewModel();
                 var productsAfterAdd = productService.GetAllProducts();
                 var produitTest2 = productsAfterAdd.FirstOrDefault(p => p.Name == "ProductTest2");
                 Assert.NotNull(produitTest2);
@@ -131,6 +134,62 @@ namespace P3AddNewFunctionalityDotNetCore.TestsIntegrat
                 var productsAfterDelete = productService.GetAllProducts();
                 var verifProduitTest2 = productsAfterDelete.FirstOrDefault(p => p.Name == "ProductTest2");
                 Assert.Null(verifProduitTest2);
+            }
+        }
+
+        [Fact]
+        [Description("I add a product on the admin side, I delete it and check that it has been deleted")]
+
+        public void AddProductInStockAndInOrder()
+        {
+            //Arrange
+            var cart = new Cart();
+
+            // Récupérer la chaîne de connexion depuis appsettings.json
+            var connectionString = _configuration.GetConnectionString("P3Referential");
+
+            // Utiliser la chaîne de connexion pour créer une instance du contexte de base de données
+            var options = new DbContextOptionsBuilder<P3Referential>()
+                .UseSqlServer(connectionString)
+                .Options;
+
+            //using et ce qu'il y a dans l'accolade permet de libéré les connexions à la BDD
+            using (var context = new P3Referential(options, _configuration))
+            {
+                // Créer une instance du ProductService en utilisant le contexte de la base de données
+                var productService = new ProductService(cart, new ProductRepository(context), null, null);
+
+                // Créer une instance du ProductController en utilisant le ProductService
+                var productController = new ProductController(productService);
+
+                // Act
+                var createActionResult = productController.Create(new ProductViewModel
+                {
+                    Name = "ProductTest3",
+                    Description = "test d'intégration 3",
+                    Stock = "3",
+                    Details = "test3",
+                    Price = "3"
+                });
+
+                var productsAfterAdd = productService.GetAllProducts();
+                var produitTest3 = productsAfterAdd.FirstOrDefault(p => p.Name == "ProductTest3");
+
+                cart.AddItem(produitTest3, 1);
+
+                // Assert
+                bool PresenceProduitTest3 = false;
+                foreach (var l in cart.Lines)
+                {
+                    if (l.Product == produitTest3)
+                        PresenceProduitTest3 = true;
+                }
+                Assert.True(PresenceProduitTest3);
+
+                //Nettoyage de la base de donnée du produit créé
+                cart.RemoveLine(produitTest3);
+                productController.DeleteProduct(produitTest3.Id);
+
             }
         }
     }
